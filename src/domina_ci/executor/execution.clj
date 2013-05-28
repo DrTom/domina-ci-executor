92a75df24bd6a732f7d494352f4f1bb8bb04385e
Thomas Schank <DrTom@schank.ch>
2013-05-28 14:47:12 +0200

    Improved execution

--- src/domina_ci/executor/execution.clj
+++ src/domina_ci/executor/execution.clj
@@ -2,7 +2,27 @@
   (:require 
     [clojure.pprint :as pprint]
     [domina-ci.executor.core :as core]
-    ))
+    )
+  (:import 
+    [java.io File ])
+  )
+
+(defn execute-script [command script handler]
+  (let [ script-file (File/createTempFile "domina_", ".script") ]
+    (pprint/pprint script-file)
+    (.deleteOnExit script-file)
+    (spit script-file script)
+    (let [process (. (Runtime/getRuntime) exec 
+                     (into-array [command (.getAbsolutePath script-file)])) ]
+      (.start (Thread. (fn []
+                         (.waitFor process)
+                         (handler 
+                           {:exit-value (.exitValue process)
+                            :stdout (slurp (.getInputStream process))
+                            :stderr (slurp (.getErrorStream process))
+                            }))))
+      process
+      )))
 
 (defn create-and-process-execution [params result-handler]
   (let [uuid (:uuid params)]
@@ -11,7 +31,7 @@
            uuid params)
 
     (let [
-          process (. (Runtime/getRuntime) exec "ls -lah")
+          process (. (Runtime/getRuntime) exec (:command params))
           stdout (slurp (.getInputStream process))
           stderr (slurp (.getErrorStream process))
           exit-value (.exitValue process)
@@ -20,9 +40,9 @@
                   :command_stderr stderr}
           ]
 
-;      (swap! core/executions
-;             (fn [executions uuid result]
-;               (conj executions {uuid (conj (executions uuid) result)})
-;               uuid result))
-;      
+      (swap! core/executions
+             (fn [executions uuid result]
+               (conj executions {uuid (conj (executions uuid) result)}))
+               uuid result)
+      
       )))
