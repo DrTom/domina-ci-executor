4ecce0a25227945f12f276212e7d47d7b9d41061
Thomas Schank <DrTom@schank.ch>
2013-05-28 17:38:34 +0200

    wip

--- src/domina_ci/executor/execution.clj
+++ src/domina_ci/executor/execution.clj
@@ -2,27 +2,46 @@
   (:require 
     [clojure.pprint :as pprint]
     [domina-ci.executor.core :as core]
+    [clojure.stacktrace :as stacktrace]
     )
   (:import 
     [java.io File ])
   )
 
-(defn execute-script [command script handler]
-  (let [ script-file (File/createTempFile "domina_", ".script") ]
-    (pprint/pprint script-file)
-    (.deleteOnExit script-file)
-    (spit script-file script)
-    (let [process (. (Runtime/getRuntime) exec 
-                     (into-array [command (.getAbsolutePath script-file)])) ]
-      (.start (Thread. (fn []
-                         (.waitFor process)
-                         (handler 
-                           {:exit-value (.exitValue process)
-                            :stdout (slurp (.getInputStream process))
-                            :stderr (slurp (.getErrorStream process))
-                            }))))
-      process
-      )))
+(defn- handle-process-exception [handler e]
+  (handler 
+    {:exit-value -1
+     :stdout nil
+     :stderr (with-out-str (stacktrace/print-stack-trace e))
+     }))
+
+(defn execute-script [interpreter script handler & more]
+  "runs the script in the interpreter by passing the full path as the first
+  argument; returns a process object (or nil) immediatelly ;  the handler is
+  called from a different thread after the interpreter terminates; the handler
+  takes one argument, a map with the keys :exit-value, :stdout, :stderr;" 
+  (try 
+    (let [ script-file (File/createTempFile "domina_", ".script") ]
+      (pprint/pprint script-file)
+      (.deleteOnExit script-file)
+      (spit script-file script)
+      (try 
+        (let [process (. (Runtime/getRuntime) exec 
+                         (into-array (flatten (conj interpreter [(.getAbsolutePath script-file)])))
+                         (into-array ["X=Y"])
+                         ) ]
+          (.start (Thread. (fn []
+                             (try (do (.waitFor process)
+                                      (handler 
+                                        {:exit-value (.exitValue process)
+                                         :stdout (slurp (.getInputStream process))
+                                         :stderr (slurp (.getErrorStream process))
+                                         }))
+                                  (catch Exception e
+                                    (handle-process-exception handler e))))))
+          process)))
+      (catch Exception e (.start (Thread. #(handle-process-exception handler e))))))
+
 
 (defn create-and-process-execution [params result-handler]
   (let [uuid (:uuid params)]
