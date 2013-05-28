f6a9d49878c9068d8271fccb1ff7485cd61c3417
Thomas Schank <DrTom@schank.ch>
2013-05-28 20:49:24 +0200

    Finished execute-script

--- src/domina_ci/executor/execution.clj
+++ src/domina_ci/executor/execution.clj
@@ -15,32 +15,51 @@
      :stderr (with-out-str (stacktrace/print-stack-trace e))
      }))
 
-(defn execute-script [interpreter script handler & more]
-  "runs the script in the interpreter by passing the full path as the first
-  argument; returns a process object (or nil) immediatelly ;  the handler is
-  called from a different thread after the interpreter terminates; the handler
-  takes one argument, a map with the keys :exit-value, :stdout, :stderr;" 
+(def defaul-system-interpreter
+  (condp = (clojure.string/lower-case (System/getProperty "os.name"))
+    "windows" ["cmd.exe" "/c"]
+    ["bash" "-l"]))
+
+(defn execute-script 
+  [script handler & {:keys [working-dir env-variables interpreter] 
+                     :or {env-variables [], 
+                          interpreter defaul-system-interpreter
+                          working-dir (java.io.File. (System/getProperty "user.home"))
+                          }}]
+  "Runs the script in the (default) interpreter by passing the full path as the
+  last argument. Returns a process object (or nil) immediatelly.  The handler
+  is always called from a different thread.  The handler takes one argument, a
+  map with the keys: :exit-value, :stdout, and :stderr.
+
+  Named options are: 
+  * The interpreter, which is an array of the interpreter path and arguments, defaults 
+  to [\"cmd.exe\" \"/c\"] in windows and [\"bash\" \"-l\"] in unixes.
+  * env-variables, an array of strings like [\"LEVEL=DEBUG\"], defaults to []." 
+
   (try 
     (let [ script-file (File/createTempFile "domina_", ".script") ]
-      (pprint/pprint script-file)
       (.deleteOnExit script-file)
       (spit script-file script)
-      (try 
-        (let [process (. (Runtime/getRuntime) exec 
-                         (into-array (flatten (conj interpreter [(.getAbsolutePath script-file)])))
-                         (into-array ["X=Y"])
-                         ) ]
-          (.start (Thread. (fn []
-                             (try (do (.waitFor process)
-                                      (handler 
-                                        {:exit-value (.exitValue process)
-                                         :stdout (slurp (.getInputStream process))
-                                         :stderr (slurp (.getErrorStream process))
-                                         }))
-                                  (catch Exception e
-                                    (handle-process-exception handler e))))))
-          process)))
-      (catch Exception e (.start (Thread. #(handle-process-exception handler e))))))
+      (let 
+        [cmdarray (into-array (flatten (conj interpreter [(.getAbsolutePath script-file)])))
+         envp (condp = (count env-variables)
+                0 nil
+                (into-array env-variables))]
+        (try 
+          (let [process (. (Runtime/getRuntime) exec cmdarray envp working-dir) ]
+            (.start (Thread. (fn []
+                               (try (do (.waitFor process)
+                                        (handler 
+                                          {:exit-value (.exitValue process)
+                                           :stdout (slurp (.getInputStream process))
+                                           :stderr (slurp (.getErrorStream process))
+                                           }))
+                                    (catch Exception e
+                                      (handle-process-exception handler e))))))
+            process))))
+    (catch Exception e (.start (Thread. #(handle-process-exception handler e))))))
+
+
 
 
 (defn create-and-process-execution [params result-handler]
